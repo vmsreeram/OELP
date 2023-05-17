ea3221a1149487c70d5900456401756ddda6d307
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-05-17 23:06:56 +0530

    .

--- csv_reader/index.js
+++ csv_reader/index.js
@@ -1,17 +1,19 @@
 const { ipcRenderer } = require('electron');
 const Papa = require('papaparse');
-// const usbDetect = require('usb-detection');
+const usb = require('usb');
 const openCsvButton = document.getElementById('open-csv-button');
 const csvContent = document.getElementById('csv-content');
-// const saveToUsbButton = document.getElementById('save-to-usb-button');
+const saveToUsbButton = document.getElementById('save-to-usb-button');
 
+let csvFilePath = '';
 
 openCsvButton.addEventListener('click', async () => {
   try {
     const result = await ipcRenderer.invoke('open-csv-dialog');
     if (result) {
       csvContent.value = result;
-      saveToUsbButton.disabled = false;
+      csvFilePath = result.filePath;
+      // saveToUsbButton.disabled = false;
     } else {
       csvContent.value = '';
       saveToUsbButton.disabled = true;
@@ -23,38 +25,42 @@ openCsvButton.addEventListener('click', async () => {
   }
 });
 
-// saveToUsbButton.addEventListener('click', async () => {
-//     try {
-//       const usbDevices = await usbDetect.find();
-//       const usbDrive = usbDevices.find((device) => device.isUSB && !device.isMounted);
-//       if (usbDrive) {
-//         const csvData = await fs.promises.readFile(selectedFilePath, { encoding: 'utf-8' });
-//         const csvFileName = selectedFilePath.split(/[\\/]/).pop();
-//         const usbPath = usbDrive.mountpoints[0].path;
-//         const csvFilePath = `${usbPath}\\${csvFileName}`;
-//         await fs.promises.writeFile(csvFilePath, csvData, { encoding: 'utf-8' });
-//         alert(`CSV file saved to USB drive ${usbDrive.deviceName}!`);
-//       } else {
-//         alert('No unmounted USB drives found.');
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       alert('Error saving CSV file to USB drive.');
-//     }
-//   });
-
+saveToUsbButton.addEventListener('click', async () => {
+  try {
+    const devices = usb.getDeviceList();
+    const usbDrive = devices.find((device) => true || (device.deviceDescriptor.idVendor === 0x0781 && device.deviceDescriptor.idProduct === 0x558a));
+    if (usbDrive) {
+      usbDrive.open();
+      const interface = usbDrive.interfaces[0];
+      interface.claim();
+      const endpoint = interface.endpoints[0];
+      const csvData = await fs.promises.readFile(csvFilePath, { encoding: 'utf-8' });
+      const csvFileName = path.basename(csvFilePath);
+      endpoint.transfer(csvData, (error) => {
+        if (error) {
+          console.error(error);
+          alert('Error saving CSV file to USB drive.');
+        } else {
+          alert(`CSV file saved to USB drive ${usbDrive.deviceDescriptor.iManufacturer} ${usbDrive.deviceDescriptor.iProduct}!`);
+        }
+        interface.release();
+        usbDrive.close();
+      });
+    } else {
+      alert('No suitable USB drives found.');
+    }
+  } catch (error) {
+    console.error(error);
+    alert('Error saving CSV file to USB drive.');
+  }
+});
 
 ipcRenderer.on('csv-data', (event, csvData) => {
-    console.log("aiofdf")
-    if (csvData) {
-        // console.log("here")
-        // const columnNames = ['Column 1', 'Column 2', 'Column 3','Column 4']; // Replace with your column names
-        // const parsedData = Papa.parse(csvData, { header: false, delimiter: ',', columns: columnNames });
-        // console.log(parsedData)
-        // const formattedData = JSON.stringify(parsedData.data, null, 2);
-        csvContent.value = csvData;
-      }
-    else {
-      csvContent.value = '';
-    }
-  });
+  if (csvData) {
+    csvContent.value = csvData;
+    // saveToUsbButton.disabled = false;
+  } else {
+    csvContent.value = '';
+    saveToUsbButton.disabled = true;
+  }
+});

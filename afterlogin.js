c7f39e89649972d99fcecae8120c738165ac7428
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-05-18 11:16:16 +0530

    user tests integrated

--- afterlogin.js
+++ afterlogin.js
@@ -1,9 +1,10 @@
 // interprocess communication is done using ipcRenderer. 
 const { ipcRenderer } = require('electron');
-const wifi = require('node-wifi');
+
 const { MongoClient } = require("mongodb");
 const uri = "mongodb://0.0.0.0:27017/";
 const client = new MongoClient(uri);
+
 // import { BrowserWindow } from 'electron';
 
 
@@ -25,7 +26,7 @@ async function updateCreditsDisplay() {
   updateCreditsDisplay();
   
 let test1_amount = 100;
-let test2_amount = 100;
+
 async function testSetFn1() {
     // alert("Alert: To be implemented");
     var db1 = client.db("tempdemo")
@@ -35,8 +36,6 @@ async function testSetFn1() {
             { name: 'credits' },
             { $set: { amount: amount_left.amount-test1_amount } }
           );
-          updateCreditsDisplay();
-
     ipcRenderer.send('user_test1');
 
     }
@@ -51,38 +50,11 @@ async function testSetFn1() {
      */
 }
 
-
-
-async function testSetFn2() {
-    wifi.init();
-    wifi.getCurrentConnections(async (err, currentConnections) => {
-        try {
-            console.log("inside");
-          if (currentConnections.length === 0) {
-           alert("Wifi needed for this test");
-          } else {
-            const amount_left = await db1.collection("credits").findOne({ "name":"credits" });
-            if(test2_amount <= amount_left.amount){
-                const result = await db1.collection('credits').updateOne(
-                    { name: 'credits' },
-                    { $set: { amount: amount_left.amount-test1_amount } }
-                  );
-                  alert("test completed");
-                }
-                else{
-                    alert("Insufficient credits")
-                }
-                  updateCreditsDisplay();
-            
-          }
-        } catch (e) {
-          console.log(e)
-        }
-      });
-      console.log("here")
-  }
-
-
+  
+function testSetFn2() {
+    alert("Alert: To be implemented");
+    // ipcRenderer.send('close')
+}
 function testSetFn3() {
     alert("Alert: To be implemented");
     // ipcRenderer.send('close')

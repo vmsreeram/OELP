18e0ef39ba1d223f05b0a46c74382e4af42a79df
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-05-18 10:42:45 +0530

    sim-server upd with save

--- sim-server/renderer.js
+++ sim-server/renderer.js
@@ -1,4 +1,7 @@
 const { ipcRenderer } = require('electron');
+const fs = require('fs');
+
+const moveHistory = [];
 
 ipcRenderer.on('move', (event, { x, y, w, c }) => {
   const dot = document.getElementById('dot');
@@ -8,8 +11,31 @@ ipcRenderer.on('move', (event, { x, y, w, c }) => {
   dot.style.height = w + 'px';
   dot.style.backgroundColor = c;
 
+  moveHistory.push({ x, y, w, c });
   console.log(`width_set to ${w}`);
   console.log("MOVED__");
 });
-const { ipcRenderer } = require('electron');
+// const { ipcRenderer } = require('electron');
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', () => {
+  const csv = convertToCSV(moveHistory);
+  const filename = 'move_history.csv';
+  saveFile(csv, filename);
+});
 
+function convertToCSV(arr) {
+  const header = Object.keys(arr[0]).join(',');
+  const rows = arr.map(obj => Object.values(obj).join(','));
+  return [header, ...rows].join('\n');
+}
+
+function saveFile(data, filename) {
+  fs.writeFile(filename, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    alert(`File saved as ${filename}`);
+    console.log(`File saved as ${filename}`);
+  });
+}

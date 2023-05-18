0bcd136a2f753f61317797338219213a5744e870
vmsreeram <112001051@smail.iitpkd.ac.in>
2023-05-18 12:48:19 +0530

    log

--- afterlogin.js
+++ afterlogin.js
@@ -106,6 +106,21 @@ async function testSetFn1() {
 }
 */
 
+function getCurrentTimestamp() {
+  const timestamp = Date.now();
+  const dateObj = new Date(timestamp);
+
+  const day = dateObj.getDate().toString().padStart(2, '0');
+  const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+  const year = dateObj.getFullYear().toString().slice(-2);
+  const hours = dateObj.getHours().toString().padStart(2, '0');
+  const minutes = dateObj.getMinutes().toString().padStart(2, '0');
+  const seconds = dateObj.getSeconds().toString().padStart(2, '0');
+
+  const formattedTimestamp = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  return formattedTimestamp;
+}
+
 async function testSetFn1() {
   var db1 = client.db("tempdemo");
   const amount_left = await db1.collection("credits").findOne({ "name": "credits" });
@@ -117,7 +132,56 @@ async function testSetFn1() {
         { $set: { amount: amount_left.amount - test1_amount } }
       );
       updateCreditsDisplay();
-      ipcRenderer.send('user_test1');
+      ipcRenderer.send('getuser');
+      ipcRenderer.on('user-details',(event,sessionuser)=>{
+        console.log('sessionuser=');
+        console.log(sessionuser);
+        alert('sessionuser='+sessionuser)
+        ///
+        
+
+        const fs = require('fs');
+
+        function convertToCSV(arr) {
+          const header = Object.keys(arr[0]).join(',');
+          const rows = arr.map(obj => Object.values(obj).join(','));
+          return [rows].join('\n') + '\n';;
+        }
+
+        function saveFile(data, filename) {
+          return new Promise((resolve, reject) => {
+            fs.appendFile(filename, data, (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                console.log(`File saved as ${filename}`);
+                resolve();
+              }
+            });
+          });
+        }
+
+        async function onCloseWindow() {
+          const userLogs = [
+            { user: `${sessionuser}`,test: 'test 1',  used_credit: test1_amount, timeStamp: getCurrentTimestamp() },
+          ];
+          const csv = convertToCSV(userLogs);
+          const filename = 'userLogs.csv';
+          try {
+            await saveFile(csv, filename);
+            // Destroy the window here
+            ipcRenderer.send('user_test1');
+            //
+          } catch (error) {
+            console.error(error);
+          }
+        }
+       onCloseWindow();
+        ///
+      });
+
+
+      // ipcRenderer.send('user_test1');
     } else {
       alert("HTTP server is not running.");
     }
@@ -140,6 +204,55 @@ function testSetFn2() {
                     { name: 'credits' },
                     { $set: { amount: amount_left.amount-test1_amount } }
                   );
+                  ////
+                  ipcRenderer.send('getuser');
+                  ipcRenderer.on('user-details',(event,sessionuser)=>{
+                    console.log('sessionuser=');
+                    console.log(sessionuser);
+                    alert('sessionuser='+sessionuser)
+                    ///
+                    
+
+                    const fs = require('fs');
+
+                    function convertToCSV(arr) {
+                      const header = Object.keys(arr[0]).join(',');
+                      const rows = arr.map(obj => Object.values(obj).join(','));
+                      return [rows].join('\n') + '\n';;
+                    }
+
+                    function saveFile(data, filename) {
+                      return new Promise((resolve, reject) => {
+                        fs.appendFile(filename, data, (err) => {
+                          if (err) {
+                            reject(err);
+                          } else {
+                            console.log(`File saved as ${filename}`);
+                            resolve();
+                          }
+                        });
+                      });
+                    }
+
+                    async function onCloseWindow() {
+                      const userLogs = [
+                        { user: `${sessionuser}`,test: 'test 2', used_credit: test2_amount, timeStamp: getCurrentTimestamp() },
+                      ];
+                      const csv = convertToCSV(userLogs);
+                      const filename = 'userLogs.csv';
+                      try {
+                        await saveFile(csv, filename);
+                        // Destroy the window here
+                        // ipcRenderer.send('user_test1');
+                        //
+                      } catch (error) {
+                        console.error(error);
+                      }
+                    }
+                  onCloseWindow();
+                    ///
+                  });
+                  ////
                   alert("test completed");
                 }
                 else{
